0eda294f6723941f4131d63ceec6c5c7f71f1ed1
Gladstone Rose <gladstonerose@hotmail.com>
2022-07-14 03:48:16 -0500

    add Blocks and scope lessons

---
+++ BLOCKS_AND_SCOPE/block_scope.js
@@ -0,0 +1,8 @@
+const logVisibleLightWaves = () => {
+  let lightWaves = "Moonlight";
+  console.log(lightWaves);
+};
+let lightWaves = "Moonlight";
+//logVisibleLightWaves();
+
+console.log(lightWaves);
